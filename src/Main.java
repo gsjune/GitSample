e9a51eb9624a64b421be9e2c8b2ee1a412b0d5eb
Hongchul Kim <gsjune@naver.com>
2017-07-12 06:08:22 +0900

    안녕

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("안녕"); // push는 Find Action에서
     }
 }
+// push는 Find Action에서
+// fetch는 Find Action에서
