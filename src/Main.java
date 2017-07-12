a4081db0c555c4ce275b75d37dcf14ca86b8a5f5
Hongchul Kim <gsjune@naver.com>
2017-07-12 10:22:35 +0900

    hello

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("안녕"); // push는 Find Action에서
+        System.out.println("hello");
     }
 }
 // push는 Find Action에서
